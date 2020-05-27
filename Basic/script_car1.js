0f5543fcba1ef2ff8785cf6cc0b5ce6848d83f5f
Carina Jurgovsky <carina.jurgovsky@gmx.at>
2020-05-27 13:27:15 +0200

    new file script car1 js

---
+++ Basic/script_car1.js
@@ -0,0 +1,8 @@
+function box_volume_and_area (width, height, depth) {
+	return width * height
+	return width * height * depth
+}
+ var area = box_volume_and_area(20,25);
+ var volume = box_volume_and_area (20,25,10);
+ document.write("The area of the box is = " + area +"<br>"); 
+ document.write ("The volume of the box is = " + volume);
