f0ce2b6afb296bfcd33008d6cbed57b6714ba65f
Ahmed Abdelaal <ahmed.abdellaoue@gmail.com>
2020-05-27 13:22:42 +0200

    Changed little error

--- Basic/script_car.js
+++ Basic/script_car.js
@@ -1,8 +1,9 @@
 function box_volume_and_area (width, height, depth) {
-	return width * height
-	return width * height * depth
+	var area = width * height
+	var volume = width * height * depth
+	document.write("The area of the box is = " + area + "<br>"); 
+    document.write ("The volume of the box is = " + volume);
 }
- var area = box_volume_and_area(20,25);
- var volume = box_volume_and_area (20,25,10);
- document.write("The area of the box is = " + area); 
- document.write ("The volume of the box is = " + volume);
+
+
+box_volume_and_area (20, 25, 10);
