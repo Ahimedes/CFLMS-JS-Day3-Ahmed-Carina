17a75acb0ab4e1a3022f5b0d03ba43fa0e9123e5
Ahmed Abdelaal <ahmed.abdellaoue@gmail.com>
2020-05-27 10:31:08 +0200

    First exercise

---
+++ Basic/index1.js
@@ -0,0 +1,6 @@
+function Crystalgazer(job_title, location, partners_name, number_children)
+	{
+	document.write("You will be a " + job_title + " in " + location + " and married to " + partners_name + " with " + number_children + " kids.")	
+}
+
+Crystalgazer("Developer", "Vienna", "somebody", "3")
