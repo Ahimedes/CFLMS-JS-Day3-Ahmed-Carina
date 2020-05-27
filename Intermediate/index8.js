e6dbce781eb146c0e996c8839ea0a44839c58762
Ahmed Abdelaal <ahmed.abdellaoue@gmail.com>
2020-05-27 15:36:10 +0200

    Fixed comma in exercise 8

--- Intermediate/index8.js
+++ Intermediate/index8.js
@@ -1,8 +1,9 @@
 function averageGrade(Mathe, Physics, English){
 	var sum = Mathe + Physics + English
-	var avg = sum / 3
+	var a = sum / 3
+	var avg = a.toFixed(0);
 	document.write("Sum: " + sum + "<br>")
 	document.write("Average: " + avg)
 }
 
-averageGrade(3, 4, 6)
+averageGrade(3, 5, 5)
